71791db004d47950854af379966af6d11ed17b7d
DmiKol <dmikol13@mail.ru>
2020-02-17 14:57:15 +0300

    feat: add solution carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,8 +1,13 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  
+module.exports = function dateSample(sampleActivity) {
 
-  
+if(typeof(sampleActivity) == 'string' && parseFloat(sampleActivity) > 0 && parseFloat(sampleActivity) < MODERN_ACTIVITY){
+
+return result = Math.ceil((Math.log(MODERN_ACTIVITY/(parseFloat(sampleActivity))))/(0.693/HALF_LIFE_PERIOD));
+
+}
+
+return false;
 };
