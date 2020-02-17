7d23515e458c21edb4aacf4419e74ad003119d12
DmiKol <dmikol13@mail.ru>
2020-02-17 13:41:39 +0300

    feat: add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+
+if(Array.isArray(members)){
+let result = members.map(function(item){
+if(typeof(item) == 'string'){
+return item[(item.search(/[a-z]/i))].toUpperCase();
+}
+});
+
+return result.sort().join('');
+}
+return false;
+}
