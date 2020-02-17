c5edaf6cd41f1dc05497b6ba98576ebcc96b29d7
DmiKol <dmikol13@mail.ru>
2020-02-17 19:02:16 +0300

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,22 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    
+let arrStr = [];
+let arrAdd = [];
+
+if(Object.keys(options).length == 1){
+    options['separator'] = '+';
+}
+
+for(let i = 0; i < options['additionRepeatTimes']; i++){
+arrAdd.push(""+options['addition']+"");
+}
+for(let i = 0; i < options['repeatTimes']; i++){
+arrStr.push(str+arrAdd.join(""+options['additionSeparator']+""));
+}
+if(arrStr.length == 0 && arrAdd.length == 0){
+return str+options['addition'];  
+}
+return arrStr.join(""+options['separator']+"");
+
 };
   
