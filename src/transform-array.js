127ca1aa38f67ca7bdc43880815f25ab227ad366
DmiKol <dmikol13@mail.ru>
2020-02-19 10:33:15 +0300

    feat: add solution hanoi-tower

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,35 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let result = [];
+  
+    
+    arr.forEach(function(item, index){
+        if(item == '--discard-next' && (arr[index+1] !== undefined)){
+            result.push('lol');
+        }
+        if(item == '--discard-prev' && result.length > 0){
+            result.pop();
+        }
+        else if(item == '--double-next' && (arr[index+1] !== undefined)){
+            result.push(arr[index+1]);
+        }
+        else if(item == '--double-prev' && (arr[index-1] !== undefined)){
+            result.push(arr[index-1]);
+        }else if(item != '--discard-next' && item != '--discard-prev' && item != '--double-next' && item != '--double-prev'){
+            result.push(item);
+        }
+
+    });
+    let res = [];
+    let n = 0;
+result.forEach(function(item, index){
+  
+if(item == "lol"){
+        n = 2;  
+}else if(n == 2){
+        n = 0;
+}else{
+    res.push(item);
+}
+});
+return res;
 };
