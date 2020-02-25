2c427173fb3098d88d462d637aeccdb4937fc2d5
DmiKol <dmikol13@mail.ru>
2020-02-25 16:17:19 +0300

    fix: solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,9 +1,22 @@
 class VigenereCipheringMachine {
+    constructor(direct = true) {
+        this.direct = direct;
+    }
+
     encrypt(word, key) {
+        return this.crypt(word, key, false);
+    }
+
+    decrypt(word, key) {
+        return this.crypt(word, key, true);
+    }
+
+    crypt(word, key, isDec) {
 
         if(!word || !key){
             throw new Error();
         }
+        
         let subKey = [];
         if(word.search(/[a-z]/gi) == -1){
             
@@ -26,7 +39,8 @@ class VigenereCipheringMachine {
              
              for(let n = 0; n < word.length; n++){
                  if(word[n].search(/[a-z]/gi) != -1){
-                     arrCode.push(String.fromCharCode((((word[n].toLowerCase().charCodeAt()-97)+(subKey[0].charCodeAt()-97)) % 26)+97));
+                     !isDec ? arrCode.push(String.fromCharCode((((word[n].toLowerCase().charCodeAt()-97)+(subKey[0].charCodeAt()-97)) % 26)+97))
+                     : arrCode.push(String.fromCharCode((((word[n].toLowerCase().charCodeAt()-97)+26-(subKey[0].charCodeAt()-97)) % 26)+97));
                      subKey.shift();
       
                  }else{
@@ -34,44 +48,13 @@ class VigenereCipheringMachine {
                  }
              }
     
+             if(!this.direct) {
+                arrCode.reverse();
+            }
+
              return arrCode.join('').toUpperCase();
         
     }
-    
-        decrypt(word, key) {
-            if(!word || !key){
-                throw new Error();
-            }
-            let subKey = [];
-            let z = 0;
-            let lengthSubKey = (word.match(/[a-z]/gi)).length;
-            
-            for(let m = 0; m < lengthSubKey; m++){
-                if(key[m-(key.length)*z]){
-                            subKey.push((key[m-(key.length)*z]).toLowerCase()); 
-                        }else{
-                            z++;
-                            subKey.push((key[m-(key.length)*z]).toLowerCase()); 
-                        }
-            } 
-    
-             let arrCode =[];
-             
-             for(let n = 0; n < word.length; n++){
-                 if(word[n].search(/[a-z]/gi) != -1){
-                     arrCode.push(String.fromCharCode((((word[n].toLowerCase().charCodeAt()-97)+26-(subKey[0].charCodeAt()-97)) % 26)+97));
-                     subKey.shift();
-                 }else{
-                    arrCode.push(word[n]);
-                 }
-             }
-             return arrCode.join('').toUpperCase();
-
-
-        }
-
-        
-    
     
 }
 
